0d8ec6e09fa37fdfd0b7c7a2679aadd25542f9d4
Nazmul Alam <nazmuldipu@gmail.com>
2020-10-01 12:00:22 +0600

    navbar added

---
+++ src/shared/data/category.ts
@@ -0,0 +1,127 @@
+export const CategoryTree = {
+    category: [
+        {
+            name: 'Frozen',
+            slug: 'frozen',
+            sub_category: [
+                {
+                    name: 'Bigger Packs', slug: 'bigger-packs', sub_sub_category: []
+                },
+                {
+                    name: 'Meat & Chicken', slug: 'meat-&-chicken',
+                    sub_sub_category: [
+                        { name: 'Chicken', slug: 'chicken' },
+                        { name: 'Bread & Butter Chicken', slug: 'bread-&-butter-chiken' },
+                        { name: 'Burger', slug: 'burger' },
+                        { name: 'Sausages', slug: 'sausages' },
+                        { name: 'Beef', slug: 'beef' },
+                        { name: 'Lamb', slug: 'lamb' },
+                        { name: 'Lean Meat', slug: 'lean-meat' },
+                        { name: 'Turkey', slug: 'turkey' },
+                    ]
+                },
+                {
+                    name: 'Ice Cream & Lollies', slug: 'ice-cream-&-lollies',
+                    sub_sub_category: [
+                        { name: 'Ice Cream Cone', slug: 'ice-cream-cone' },
+                        { name: 'Ice Lollies', slug: 'ice-lollies' },
+                        { name: 'Ice Cream Tubs', slug: 'ice-cream-tubs' },
+                    ]
+                },
+                {
+                    name: 'Ready Meals', slug: 'ready-meals',
+                    sub_sub_category: [
+                        { name: 'Indian', slug: 'indian' },
+                        { name: 'Chinese', slug: 'chinese' },
+                        { name: 'Italian', slug: 'italian' },
+                    ]
+                },
+                {
+                    name: 'Vegetables & Rice', slug: 'vegetables-&-rice',
+                    sub_sub_category: [
+                        { name: 'Vegetables', slug: 'vegetables' },
+                        { name: 'Rice & Grains', slug: 'rice-&-grains' },
+                        { name: 'Vegetable Side Dishes', slug: 'vegetable-side-dishes' },
+                        { name: 'Herbs', slug: 'herbs' },
+                    ]
+                }
+
+            ],
+        },
+        {
+            name: 'Fresh',
+            slug: 'fresh',
+            sub_category: [
+                {
+                    name: 'Fruit, Veg & Salads', slug: 'fruit-veg-&-salads',
+                    sub_sub_category: [
+                        { name: 'Fruits', slug: 'fruits' },
+                        { name: 'Salads', slug: 'salads' },
+                        { name: 'Vegetables', slug: 'vegetables' },
+                        { name: 'Potatoes', slug: 'potatoes' },
+                    ]
+                },
+                {
+                    name: 'Milk, Butter & Eggs', slug: 'milk-butter-&-eggs',
+                    sub_sub_category: [
+                        { name: 'Milk', slug: 'milk' },
+                        { name: 'Butter', slug: 'butter' },
+                        { name: 'Eggs', slug: 'eggs' },
+                        { name: 'Cream', slug: 'cream' },
+                    ]
+                },
+                {
+                    name: 'Cheese', slug: 'cheese',
+                    sub_sub_category: [
+                        { name: 'Cheddar Cheese', slug: 'cheddar-cheese' },
+                        { name: 'Grated & Sliced Cheese', slug: 'grated-&-sliced-cheese' },
+                        { name: 'Cheese Snacks & Spreads', slug: 'cheese-snacks-&-spreads' },
+                        { name: 'Speciality Cheese', slug: 'speciality-cheese' },
+                        { name: 'British Regional Cheese', slug: 'british-regional-cheese' },
+                    ]
+                },
+                {
+                    name: 'Desserts', slug: 'desserts',
+                    sub_sub_category: []
+                },
+            ]
+        },
+        {
+            name: 'Drinks',
+            slug: 'drinks',
+            sub_category: [
+                {
+                    name: 'Fizzy Drinks', slug: 'fizzy-drinks',
+                    sub_sub_category: [
+                        { name: 'Canned Drinks', slug: 'canned-drinks' },
+                        { name: 'Bottled Drinks', slug: 'bottled-drinks' },
+                        { name: 'Cola', slug: 'cola' },
+                        { name: 'Diet Drinks', slug: 'diet-drinks' },
+                    ]
+                },
+                {
+                    name: 'Beer, Cider & Ales', slug: 'beer-cider-&-ales',
+                    sub_sub_category: [
+                        { name: 'Beer', slug: 'beer' },
+                        { name: 'Cider', slug: 'cider' },
+                        { name: 'Ales', slug: 'ales' },
+                    ]
+                },
+                {
+                    name: 'Coffee', slug: 'coffee',
+                    sub_sub_category: [
+                        { name: 'Coffee Machine Pods', slug: 'coffee-machine-pods' },
+                        { name: 'Instant & Ground Coffee', slug: 'instant-&-ground-coffee' }
+                    ]
+                },
+                {
+                    name: 'Tea', slug: 'tea',
+                    sub_sub_category: [
+                        { name: 'Breakfast Tea', slug: 'breakfast-tea' },
+                        { name: 'Fruit & Herbal Tea', slug: 'fruit-&-herbal-tea' }
+                    ]
+                },
+            ]
+        }
+    ]
+}
